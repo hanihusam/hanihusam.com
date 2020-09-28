6c5963ec460fa30672872a4cc2f617744a603f02
Hani Husamuddin <hani.husam@gmail.com>
2020-09-28 09:57:49 +0700

    setup default themeProps

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,7 +1,10 @@
+import { useMemo } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { colors, fonts, breakpoints, widths, mediaQueries, shadows, GlobalStyles } from './utils'
 
-export const themeProps = {
+import { colors, fonts, breakpoints, widths, mediaQueries, shadows, GlobalStyles } from './utils'
+import useDarkMode from '~/utils/useDarkMode'
+
+const themeProps = {
   colors,
   fonts,
   widths,
@@ -10,9 +13,30 @@ export const themeProps = {
   shadows
 }
 
+const darkThemeProps = {
+  ...themeProps,
+  colors: {
+    ...themeProps.colors,
+    bgPrimary: '#434343',
+    primary: '#FFFFFF',
+    textPrimary: '#F2F2F2'
+  }
+}
+
 export const Theme: React.FC = ({ children }) => {
+  const [isDarkMode] = useDarkMode()
+
+  const currentTheme = useMemo(() => {
+    if (isDarkMode) {
+      // return darkThemeProps
+      return themeProps
+    }
+
+    return themeProps
+  }, [isDarkMode])
+
   return (
-    <ThemeProvider theme={themeProps}>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyles />
       {children}
     </ThemeProvider>
