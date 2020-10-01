961039303f973af1b80dc33b03b3ee889c6240af
Hani Husamuddin <hani.husam@gmail.com>
2020-10-01 10:13:16 +0700

    add new component LayoutRoot

---
+++ src/styles/LayoutRoot.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box } from './components'
+
+const LayoutRoot: React.FC = ({ children }) => (
+  <Box
+    as="main"
+    display="flex"
+    flexDirection="column"
+    position="relative"
+    minHeight="100vh"
+    overflowX="hidden"
+    color="textColorPrimary"
+    backgroundColor="bgPrimary"
+  >
+    {children}
+  </Box>
+)
+
+export default LayoutRoot
