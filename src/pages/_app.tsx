8e4b30e255dbd7291f2bcf232480e8fac2204c51
Hani Husamuddin <hani.husam@gmail.com>
2020-10-01 10:18:24 +0700

    remove navigation and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,18 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import styled from 'styled-components'
 
 import 'modern-normalize'
 import { Theme } from 'src/styles'
-import { Navigation } from '~/components/layout/Navigation'
-
-const LayoutRoot = styled.main`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  min-height: 100vh;
-  overflow-x: hidden;
-  padding-top: 80px;
-`
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hani Husam</title>
       </Head>
       <Theme>
-        <Navigation />
-        <LayoutRoot>
-          <Component {...pageProps} />
-        </LayoutRoot>
+        <Component {...pageProps} />
       </Theme>
     </>
   )
