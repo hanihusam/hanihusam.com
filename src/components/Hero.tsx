ca99245686a0fed607d7243ee04d7cda9f2aa856
Hani Husamuddin <hani.husam@gmail.com>
2020-10-15 18:33:29 +0700

    social icon animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 import styled from 'styled-components'
-import { Box, Column, Text, Heading, Paragraph, themeProps, UnstyledButton } from 'src/styles'
+import { Box, Column, Text, Heading, Paragraph, themeProps, UnstyledButton, UnstyledAnchor } from 'src/styles'
 import { FiDownload } from 'react-icons/fi'
+import { SiDribbble, SiGithub, SiLinkedin } from 'react-icons/si'
 
 const Root = Box.withComponent('section')
 
@@ -11,7 +12,7 @@ const HeroArea = styled(Root)`
   color: ${themeProps.colors.textColorSecondary};
 
   ${themeProps.mediaQueries.md} {
-    padding: 48px ${themeProps.space.lg}px 64px;
+    padding: 48px ${themeProps.space.lg}px 0;
   }
 `
 
@@ -31,7 +32,11 @@ const RightContentWrapper = styled(Box)`
   display: flex;
   flex-direction: column;
   text-align: left;
-  padding: 32px 0;
+  padding: 32px 0 0;
+
+  ${themeProps.mediaQueries.md} {
+    padding: 32px 0;
+  }
 `
 
 const FirstTitle = styled(Text)`
@@ -52,10 +57,16 @@ const Description = styled(Paragraph)`
 `
 
 const AvatarDesktop = styled(Box)`
+  margin: 0 32px -32px;
   display: none;
 
   ${themeProps.mediaQueries.md} {
     display: flex;
+    width: 30%;
+  }
+
+  ${themeProps.mediaQueries.lg} {
+    width: 100%;
   }
 `
 
@@ -83,7 +94,7 @@ const EmailMeButton = styled(UnstyledButton)`
   min-height: 40px;
   margin-right: ${themeProps.space.md}px;
   border-radius: 5px;
-  transition: 500ms;
+  transition: 800ms;
 
   & > a {
     color: #ffffff;
@@ -105,11 +116,11 @@ const CVButton = styled(UnstyledButton)`
   text-align: center;
   justify-content: center;
   align-items: center;
-  padding: ${themeProps.space.md}px;
+  padding: ${themeProps.space.md}px 0;
   min-width: 100px;
   min-height: 40px;
   border-radius: 5px;
-  transition: 500ms;
+  transition: 800ms;
 
   & > a {
     text-decoration: none;
@@ -121,13 +132,63 @@ const CVButton = styled(UnstyledButton)`
   }
 `
 
-const Icon = styled.i`
-  display: block;
+const Icon = styled(UnstyledAnchor)`
+  display: inline-flex;
   align-items: center;
   justify-content: center;
-  margin-right: 8px;
-  width: 16px;
-  height: 16px;
+  transition: 800ms;
+
+  &:hover {
+    transform: translate(0, -5px);
+  }
+
+  & > svg {
+    width: ${props => props.size}px;
+    color: ${props => props.theme.colors.primary};
+    height: auto;
+  }
+`
+
+const SocialLinkWrapper = styled(Box)`
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+  margin: 32px 0;
+
+  ${themeProps.mediaQueries.md} {
+    display: none;
+  }
+`
+
+const LeftContentWrapper = styled(Box)`
+  display: none;
+  flex-direction: column;
+  text-align: left;
+  padding: 32px 0;
+
+  ${themeProps.mediaQueries.lg} {
+    display: flex;
+  }
+
+  & > ${SecondTitle} {
+    padding-bottom: 32px;
+  }
+
+  & > ${FirstTitle} {
+    display: block;
+  }
+
+  & > ${SocialLinkWrapper} {
+    display: flex;
+    padding-top: 64px;
+  }
+`
+
+const Line = styled.span`
+  display: block;
+  width: 40px;
+  height: 10px;
+  background-color: ${themeProps.colors.secondary};
 `
 
 const Avatar: React.FC = () => (
@@ -145,6 +206,28 @@ const Hero: React.FC = () => (
   <HeroArea>
     <Column>
       <HeroContent>
+        <LeftContentWrapper>
+          <FirstTitle as="p" variant={800} color="secondary">
+            Hi, I am
+          </FirstTitle>
+          <SecondTitle as="h1" variant={1100} color="primary">
+            Hani
+            <br />
+            Husam
+          </SecondTitle>
+          <Line />
+          <SocialLinkWrapper>
+            <Icon href="https://linkedin.com/in/hanihusamuddin" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiLinkedin />
+            </Icon>
+            <Icon href="https://github.com/hanihusam" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiGithub />
+            </Icon>
+            <Icon href="https://dribbble.com/hanihusam" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiDribbble />
+            </Icon>
+          </SocialLinkWrapper>
+        </LeftContentWrapper>
         <Avatar />
         <RightContentWrapper>
           <FirstTitle as="p" variant={800} color="secondary">
@@ -164,13 +247,19 @@ const Hero: React.FC = () => (
               </Text>
             </EmailMeButton>
             <CVButton>
-              <Icon>
+              <Icon
+                href="https://drive.google.com/file/d/13G-efGaq8I5tveBz7X_nkYRAw2XP-3AL/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                marginRight="8px"
+                size="16"
+              >
                 <FiDownload />
               </Icon>
               <Text
                 as="a"
                 href="https://drive.google.com/file/d/13G-efGaq8I5tveBz7X_nkYRAw2XP-3AL/view?usp=sharing"
-                target="blank"
+                target="_blank"
                 rel="noopener noreferrer"
                 variant={400}
                 color="textColorPrimary"
@@ -179,6 +268,17 @@ const Hero: React.FC = () => (
               </Text>
             </CVButton>
           </CtaButtonWrapper>
+          <SocialLinkWrapper>
+            <Icon href="https://linkedin.com/in/hanihusamuddin" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiLinkedin />
+            </Icon>
+            <Icon href="https://github.com/hanihusamu" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiGithub />
+            </Icon>
+            <Icon href="https://dribbble.com/hanihusam" target="_blank" rel="noopener noreferrer" marginRight="24px" size="32">
+              <SiDribbble />
+            </Icon>
+          </SocialLinkWrapper>
         </RightContentWrapper>
       </HeroContent>
     </Column>
