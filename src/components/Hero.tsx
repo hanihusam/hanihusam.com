90cb3f56a663a2c950fdc03d1cfbd2a4a9dbf468
Hani Husamuddin <hani.husam@gmail.com>
2020-10-15 13:15:51 +0700

    setup cta button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import styled from 'styled-components'
 import { Box, Column, Text, Heading, Paragraph, themeProps, UnstyledButton } from 'src/styles'
+import { FiDownload } from 'react-icons/fi'
 
 const Root = Box.withComponent('section')
 
@@ -66,6 +67,69 @@ const AvatarMobile = styled(Box)`
   }
 `
 
+const CtaButtonWrapper = styled(Box)`
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+`
+
+const EmailMeButton = styled(UnstyledButton)`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  padding: ${themeProps.space.md}px;
+  min-width: 100px;
+  min-height: 40px;
+  margin-right: ${themeProps.space.md}px;
+  border-radius: 5px;
+  transition: 500ms;
+
+  & > a {
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  &:hover {
+    filter: brightness(80%);
+  }
+
+  ${themeProps.mediaQueries.md} {
+    min-width: 150px;
+    height: 65px;
+  }
+`
+
+const CVButton = styled(UnstyledButton)`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  padding: ${themeProps.space.md}px;
+  min-width: 100px;
+  min-height: 40px;
+  border-radius: 5px;
+  transition: 500ms;
+
+  & > a {
+    text-decoration: none;
+  }
+
+  ${themeProps.mediaQueries.md} {
+    min-width: 150px;
+    height: 65px;
+  }
+`
+
+const Icon = styled.i`
+  display: block;
+  align-items: center;
+  justify-content: center;
+  margin-right: 8px;
+  width: 16px;
+  height: 16px;
+`
+
 const Avatar: React.FC = () => (
   <>
     <AvatarMobile as="div" justifyContent="center">
@@ -93,6 +157,21 @@ const Hero: React.FC = () => (
             I am a “half-blood” Web Developer and UI Designer who has experience creating many projects in a various industry. In other
             words, I understand how to build an aesthetic, powerful, and lightweight website at once.
           </Description>
+          <CtaButtonWrapper>
+            <EmailMeButton backgroundColor="secondary" color="white" boxShadow="double">
+              <Text as="a" href="mailto:hani.husam@gmail.com" variant={400}>
+                Email Me
+              </Text>
+            </EmailMeButton>
+            <CVButton>
+              <Icon>
+                <FiDownload />
+              </Icon>
+              <Text as="a" href="mailto:hani.husam@gmail.com" variant={400} color="textColorPrimary">
+                Download CV
+              </Text>
+            </CVButton>
+          </CtaButtonWrapper>
         </RightContentWrapper>
       </HeroContent>
     </Column>
