2d955c62d8c7c0f6876dd8ff27b19e7ae3b02501
Hani Husamuddin <hani.husam@gmail.com>
2020-10-15 09:38:19 +0700

    declare Hero on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import { NextPage } from 'next'
-import Link from 'next/link'
 
 import { PageWrapper } from 'src/components/layout'
-import { CallToAction } from 'src/components'
-import { Column } from 'src/styles'
+import { CallToAction, Hero } from 'src/components'
 
 const IndexPage: NextPage = () => (
   <PageWrapper>
-    <Column>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Certe non potest. Negabat igitur ullam esse artem, quae ipsa a se
-        proficisceretur; Duo Reges: constructio interrete. Quid turpius quam sapientis vitam ex insipientium sermone pendere? Non igitur
-        bene. Vide, quaeso, rectumne sit.
-      </p>
-      <p>
-        <Link href="/about" passHref>
-          <a>About</a>
-        </Link>
-      </p>
-    </Column>
+    <Hero />
     <CallToAction />
   </PageWrapper>
 )
