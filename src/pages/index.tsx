dafce584bc8c4b230856efa66bb777e6f1c3ce42
Hani Husamuddin <hani.husam@gmail.com>
2021-01-30 22:37:34 +0700

    place about me to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { NextPage } from 'next'
 
 import { PageWrapper } from 'src/components/layout'
-import { CallToAction, Hero } from 'src/components'
+import { AboutMe, CallToAction, Hero } from 'src/components'
 
 const IndexPage: NextPage = () => (
   <PageWrapper>
     <Hero />
+    <AboutMe />
     <CallToAction />
   </PageWrapper>
 )
