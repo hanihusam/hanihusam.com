f779eacc7f4d8e1c4d9905bd6c34b7678046e4f4
Hani Husamuddin <hani.husam@gmail.com>
2021-07-28 15:03:23 +0700

    Fix Next cofig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,52 @@
-module.exports = {
-  target: process.env.BUILD_TARGET || 'server'
+const basePath = ''
+
+const nextConfig = {
+  trailingSlash: true,
+  target: 'serverless',
+  basePath,
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ]
+  }
 }
+
+module.exports = nextConfig
