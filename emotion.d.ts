4220838184a2c67fef7c4d5a97d6d938f6b284d9
Hani Husamuddin <hani.husam@gmail.com>
2021-07-28 15:03:06 +0700

    Add global emotion types

---
+++ emotion.d.ts
@@ -0,0 +1,50 @@
+import '@emotion/react'
+
+declare module '@emotion/react' {
+  export interface Theme {
+    fonts: {
+      heading: string
+      body: string
+    }
+    colors: {
+      bgPrimary: string
+      fgPrimary: string
+      bgSecondary: string
+      primary: string
+      secondary: string
+      textColorPrimary: string
+      textColorSecondary: string
+
+      buttonlightmode: string
+      buttonlightmodetext: string
+      buttondarkmode: string
+      buttondarkmodetext: string
+      navgridbgmobile: string
+
+      card: string
+      header: string
+      activeMenu: string
+    }
+    space: {
+      xxxs: number
+      xxs: number
+      xs: number
+      sm: number
+      md: number
+      lg: number
+      xl: number
+      xxl: number
+    }
+    breakpoints: string[]
+    widths: {
+      sm: number
+      md: number
+      lg: number
+      xl: number
+    }
+    shadows: {
+      single: string
+      double: string
+    }
+  }
+}
