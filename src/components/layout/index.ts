50a418e64e2285a0a5c7a5cabf21d579e24dd57b
Hani Husamuddin <hani.husam@gmail.com>
2021-07-30 09:45:45 +0700

    feat: layout component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,6 @@
 export { default as Column } from './Column'
+export { default as Container } from './Container'
+export { default as Content } from './Content'
 export { default as Footer } from './Footer'
 export { default as LayoutRoot } from './LayoutRoot'
 export { default as PageWrapper } from './PageWrapper'
