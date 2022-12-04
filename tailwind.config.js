7e6346bc4ca9010b43bf34081342122ddc750cbd
Hani Husamuddin <hani.husam@gmail.com>
2022-12-04 16:46:39 +0700

    chore: setup base color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        dark: "#46434E",
+        light: "#F2F3F2",
+        body: "#7C808B",
+        accent: "#7A8391",
+        primary: {
+          300: "#F2BB97",
+          400: "#EC9E6A",
+          500: "#E6813E",
+          600: "#D4651B",
+          700: "#A75016",
+        },
+        secondary: {
+          300: "#55749B",
+          400: "#435C7A",
+          500: "#304258",
+          600: "#1F2A38",
+          700: "#0D1117",
+        },
+      },
       fontFamily: {
         sans: ["Poppins", "Ubuntu", ...defaultTheme.fontFamily.sans],
       },
