94e8e6ed0aa80c7826dc5dfa67d2b4b9e681b21c
Hani Husamuddin <hani.husam@gmail.com>
2023-01-18 19:55:08 +0700

    feat: unset strict mode

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,15 +1,11 @@
+import { startTransition } from "react";
+
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 const hydrate = () => {
   startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
+    hydrateRoot(document, <RemixBrowser />);
   });
 };
 
