b108067fb68b1052a9edc4622aada2f964e95296
Hani Husamuddin <hani.husam@gmail.com>
2023-03-16 21:05:56 +0700

    feat: added icon and root layout

---
+++ app/components/icon-link.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const IconLink = React.forwardRef<
+  HTMLAnchorElement,
+  JSX.IntrinsicElements["a"]
+>(function IconLink(props, ref) {
+  return (
+    <a
+      {...props}
+      className={`${props.className ?? ""} duration-500 hover:-translate-y-1.5`}
+      ref={ref}
+    >
+      {props.children}
+    </a>
+  );
+});
+
+export { IconLink };
--- app/components/layout/layout-root.tsx
+++ app/components/layout/layout-root.tsx
@@ -4,7 +4,7 @@ type LayoutRootProps = {
 
 function LayoutRoot({ children }: LayoutRootProps) {
   return (
-    <main className="relative flex min-h-screen flex-col overflow-x-hidden">
+    <main className="relative flex min-h-screen flex-col overflow-x-hidden bg-light dark:bg-dark">
       {children}
     </main>
   );
