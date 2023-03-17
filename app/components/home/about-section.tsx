971f9ace0134e4a35a50eb4d61ae933c8f9656d3
Hani Husamuddin <hani.husam@gmail.com>
2023-03-17 10:51:56 +0700

    feat: added hero and about section

---
+++ app/components/home/about-section.tsx
@@ -0,0 +1,39 @@
+import { H2, H6, Paragraph } from "@/components/typography";
+
+import illustration from "../../../public/images/illustration.png";
+
+export function AboutSection() {
+  return (
+    <div className="mt-8 flex self-stretch md:mt-0 md:px-[5vw]">
+      <div className="flex w-full flex-col items-center space-y-6 self-stretch rounded-xl bg-black px-6 py-[72px] md:flex-row md:justify-between md:px-10">
+        <img
+          alt="freelancer illustration"
+          className="w-[375px] md:w-[500px]"
+          src={illustration}
+        />
+
+        <div className="flex max-w-[475px] flex-col items-start space-y-8 self-stretch">
+          <div className="flex flex-col space-y-2 self-stretch">
+            <H6 className="text-primary-500">About</H6>
+            <H2 className="text-light">Why hire me for your next project?</H2>
+          </div>
+
+          <div className="flex flex-col gap-1.5">
+            <Paragraph className="text-base">
+              I worked with a various background of client and different type of
+              product as a software engineer (frontend) or UI designer. I help
+              them to solve their business problem on the technology end.
+            </Paragraph>
+            <Paragraph className="text-base">
+              I have a principle that is &quot;stay simple and stay
+              humble&quot;. I believe, simplicity hides a great deal of
+              complexity and thoroughness. I see every project as a process of
+              solving a problem. Now it&apos;s time to solve your problems in
+              detail, in depth, and of course with simplicity.
+            </Paragraph>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/home/hero-section.tsx
@@ -0,0 +1,91 @@
+import { Button } from "@/components/button";
+import { ButtonText } from "@/components/button-text";
+import { IconLink } from "@/components/icon-link";
+import { DribbbleIcon, GithubIcon, LinkedinIcon } from "@/components/icons";
+import { H1, H2, H4, Paragraph } from "@/components/typography";
+import { AnchorOrLink } from "@/utils/misc";
+
+import avatar from "../../../public/images/avatar-hani.png";
+import avatarMemoji from "../../../public/images/avatar-memoji.png";
+
+import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
+
+export function HeroSection() {
+  return (
+    <div className="flex flex-col items-start space-y-6 self-stretch px-6 pt-6 md:flex-row md:justify-between md:px-[5vw] md:pt-12">
+      <div className="hidden flex-col space-y-16 md:flex">
+        <div className="flex flex-col space-y-6">
+          <div className="flex flex-col">
+            <H4 className="text-primary-500">Hi, I am</H4>
+            <H1 className="text-secondary-500 dark:text-light">Han</H1>
+          </div>
+
+          <div className="h-2 w-20 bg-primary-500" />
+        </div>
+
+        <div className="flex items-start space-x-6">
+          <IconLink href="https://linkedin.com/in/hanihusam/" target="_blank">
+            <LinkedinIcon className="text-secondary-500" />
+          </IconLink>
+          <IconLink href="https://github.com/hanihusam" target="_blank">
+            <GithubIcon className="text-secondary-500" />
+          </IconLink>
+          <IconLink href="https://dribbble.com/hanihusam" target="_blank">
+            <DribbbleIcon className="text-secondary-500" />
+          </IconLink>
+        </div>
+      </div>
+
+      <img
+        alt="Avatar of Han"
+        className="hidden md:block"
+        src={avatar}
+        width={523}
+      />
+      <img
+        alt="Avatar of Han"
+        className="block md:hidden"
+        src={avatarMemoji}
+        width={380}
+      />
+
+      <div className="flex w-[370px] flex-col space-y-8 self-stretch">
+        <div className="flex flex-col space-y-4">
+          <H4 className="block text-primary-500 md:hidden">
+            Hi{" "}
+            <span aria-labelledby="hi emoji" role="img">
+              👋
+            </span>
+            , I am Han
+          </H4>
+          <H2 className="text-secondary-500 dark:text-light">
+            UI Engineer based on Yogyakarta, Indonesia
+          </H2>
+        </div>
+
+        <Paragraph className="text-body">
+          I am a multi-disciplinary Frontend Developer and UI Designer who have
+          experience creating projects in a various industry and have worked
+          with diverse clients from all over the world. I merge technical and
+          design skills to create innovative product with beautiful and
+          functional user experiences.
+        </Paragraph>
+
+        <div className="flex space-x-2">
+          <AnchorOrLink to="links">
+            <Button>Reach Me</Button>
+          </AnchorOrLink>
+          <AnchorOrLink href="https://donwload.cv">
+            <ButtonText
+              className="group"
+              icon={ArrowDownTrayIcon}
+              iconClassName="duration-500 group-hover:translate-y-1.5"
+            >
+              Download CV
+            </ButtonText>
+          </AnchorOrLink>
+        </div>
+      </div>
+    </div>
+  );
+}
