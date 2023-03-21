3c8e022117ca444eb62c421c0bf0e28df747fc34
Hani Husamuddin <hani.husam@gmail.com>
2023-03-21 23:22:31 +0700

    fix: fixed index route naming follows remix v2

---
+++ app/components/home/cta-section.tsx
@@ -0,0 +1,7 @@
+export function CtaSection() {
+  return (
+    <div>
+      <h1>CTA section goes here</h1>
+    </div>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,20 @@
+import * as React from "react";
+
 import { AboutSection } from "@/components/home/about-section";
+import { CtaSection } from "@/components/home/cta-section";
 import { HeroSection } from "@/components/home/hero-section";
 import { ServicesSection } from "@/components/home/services-section";
 import { Spacer } from "@/components/spacer";
 
-export default function HomePage() {
+export default function IndexRoute() {
   return (
-    <>
+    <React.Fragment>
       <HeroSection />
       <AboutSection />
       <Spacer size="lg" />
       <ServicesSection />
       <Spacer size="lg" />
-    </>
+      <CtaSection />
+    </React.Fragment>
   );
 }
