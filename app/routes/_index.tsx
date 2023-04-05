a961bc113918ca1a9bdb8156f043aa6ac965439e
Hani Husamuddin <hani.husam@gmail.com>
2023-04-05 11:33:51 +0700

    feat: removed unfinish blog section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import { AboutSection } from "@/components/home/about-section";
-import { BlogSection } from "@/components/home/blog-section";
 import { CtaSection } from "@/components/home/cta-section";
 import { HeroSection } from "@/components/home/hero-section";
 import { ServicesSection } from "@/components/home/services-section";
@@ -14,8 +13,6 @@ export default function IndexRoute() {
       <AboutSection />
       <Spacer size="lg" />
       <ServicesSection />
-      <Spacer size="lg" />
-      <BlogSection />
       <Spacer size="lg" />
       <CtaSection />
       <Spacer size="lg" />
