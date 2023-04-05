5320bd802dcc343f8742fab5cee3e54f1b98a5b5
Hani Husamuddin <hani.husam@gmail.com>
2023-04-05 11:34:27 +0700

    feat: init comment

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { AboutSection } from "@/components/home/about-section";
+import { BlogSection } from "@/components/home/blog-section";
 import { CtaSection } from "@/components/home/cta-section";
 import { HeroSection } from "@/components/home/hero-section";
 import { ServicesSection } from "@/components/home/services-section";
@@ -13,6 +14,8 @@ export default function IndexRoute() {
       <AboutSection />
       <Spacer size="lg" />
       <ServicesSection />
+      <Spacer size="lg" />
+      <BlogSection />
       <Spacer size="lg" />
       <CtaSection />
       <Spacer size="lg" />
