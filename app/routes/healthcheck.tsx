d1712e51d5299995c85ee9f7ca04052f3d43fe53
Hani Husamuddin <hani.husam@gmail.com>
2023-06-16 14:56:15 +0700

    chore: changed fly.toml

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -12,7 +12,7 @@ export async function loader({ request }: LoaderArgs) {
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
-      prisma.like.count(),
+      prisma.contentMeta.count(),
       fetch(url.toString(), { method: "HEAD" }).then((r) => {
         if (!r.ok) return Promise.reject(r);
       }),
