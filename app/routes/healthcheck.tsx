1d7905e25e98d83cf16d9f2e6e31b11eeb30fa88
Hani Husamuddin <hani.husam@gmail.com>
2023-06-16 15:41:30 +0700

    refactor: try to fix healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -4,19 +4,14 @@ import { prisma } from "@/utils/db.server";
 import type { LoaderArgs } from "@remix-run/node";
 
 export async function loader({ request }: LoaderArgs) {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  // const host =
+  //   request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
   try {
-    const url = new URL("/", `http://${host}`);
+    // const url = new URL("/", `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      prisma.contentMeta.count(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
+    await Promise.all([prisma.contentMeta.count()]);
     return new Response("OK");
   } catch (error: unknown) {
     console.log("healthcheck ❌", { error });
