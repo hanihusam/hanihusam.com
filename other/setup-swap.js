c55cc3898c6cbd1b362f59f741982df23a108d7e
Hani Husamuddin <hani.husam@gmail.com>
2023-06-25 21:40:20 +0700

    fix: fixed await fn

--- other/setup-swap.js
+++ other/setup-swap.js
@@ -4,10 +4,13 @@ const { $ } = require("execa");
 const { writeFile } = require("node:fs/promises");
 
 console.log("setting up swapfile...");
-await $`fallocate -l 512M /swapfile`;
-await $`chmod 0600 /swapfile`;
-await $`mkswap /swapfile`;
-await writeFile("/proc/sys/vm/swappiness", "10");
-await $`swapon /swapfile`;
-await writeFile("/proc/sys/vm/overcommit_memory", "1");
-console.log("swapfile setup complete");
+$`fallocate -l 512M /swapfile`
+  .then(() => $`chmod 0600 /swapfile`)
+  .then(() => $`mkswap /swapfile`)
+  .then(() => writeFile("/proc/sys/vm/swappiness", "10"))
+  .then(() => $`swapon /swapfile`)
+  .then(() =>
+    writeFile("/proc/sys/vm/overcommit_memory", "1").finally(() => {
+      console.log("swapfile setup complete");
+    })
+  );
