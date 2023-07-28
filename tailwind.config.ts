18ce6212662371cb3c4d4da23b3b8688ef39c7d6
Hani Husamuddin <hani.husam@gmail.com>
2023-07-28 09:45:45 +0700

    refactor: how tailwind css called and set typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme.js";
 
@@ -11,26 +13,29 @@ export default {
       xl: "1440px", // this is the "design resolution"
     },
     colors: {
-      black: "#46434E",
-      white: "#FFFFFF",
-      dark: "#434343",
-      light: "#F2F2F2",
-      body: "#7C808B",
-      accent: "#7A8391",
-      base: "#E0E0E0",
+      transparent: "transparent",
+      current: "currentColor",
+      black: "var(--color-black)",
+      white: "var(--color-white)",
+      dark: "var(--color-dark)",
+      light: "var(--color-light)",
+      body: "var(--color-body)",
+      accent: "var(--color-accent)",
+      base: "var(--color-base)",
       primary: {
-        300: "#F2BB97",
-        400: "#EC9E6A",
-        500: "#E6813E",
-        600: "#D4651B",
-        700: "#A75016",
+        // color scheme is defined in /app.css
+        300: "var(--color-primary-300)",
+        400: "var(--color-primary-400)",
+        500: "var(--color-primary-500)",
+        600: "var(--color-primary-600)",
+        700: "var(--color-primary-700)",
       },
       secondary: {
-        300: "#7398C7",
-        400: "#55749B",
-        500: "#45577B",
-        600: "##304258",
-        700: "#1F2A38",
+        300: "var(--color-secondary-300)",
+        400: "var(--color-secondary-400)",
+        500: "var(--color-secondary-500)",
+        600: "var(--color-secondary-600)",
+        700: "var(--color-secondary-700)",
       },
     },
     extend: {
@@ -61,10 +66,197 @@ export default {
         "50vh": "50vh", // max height for medium size hero images
         "75vh": "75vh", // max height for giant size hero images
       },
+      typography: (theme: any) => {
+        // some fontSizes return [size, props], others just size :/
+        const fontSize = (size: string) => {
+          const result = theme(`fontSize.${size}`);
+          return Array.isArray(result) ? result[0] : result;
+        };
+
+        const breakout = {
+          marginLeft: 0,
+          marginRight: 0,
+          gridColumn: "2 / span 10",
+        };
+
+        return {
+          // DEFAULT only holds shared stuff and not the things that change
+          // between light/dark
+          DEFAULT: {
+            css: [
+              {
+                "> *": {
+                  gridColumn: "1 / -1",
+
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    gridColumn: "3 / span 8",
+                  },
+                },
+                p: {
+                  marginTop: 0,
+                  marginBottom: theme("spacing.8"),
+                  fontSize: fontSize("lg"),
+                },
+                "> div": {
+                  marginTop: 0,
+                  marginBottom: theme("spacing.8"),
+                  fontSize: fontSize("lg"),
+                },
+                a: {
+                  textDecoration: "none",
+                },
+                "a:hover,a:focus": {
+                  textDecoration: "underline",
+                  outline: "none",
+                },
+                strong: {
+                  fontWeight: theme("fontWeight.medium"),
+                  fontSize: fontSize("lg"),
+                },
+                hr: {
+                  marginTop: theme("spacing.8"),
+                  marginBottom: theme("spacing.16"),
+                },
+                pre: {
+                  color: "var(--base05)",
+                  backgroundColor: "var(--base00)",
+                  marginTop: 0,
+                  marginBottom: theme("spacing.8"),
+                  marginLeft: `-${theme("spacing.10vw")}`,
+                  marginRight: `-${theme("spacing.10vw")}`,
+                  padding: theme("spacing.8"),
+                  borderRadius: 0,
+
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    borderRadius: theme("borderRadius.lg"),
+                    ...breakout,
+                  },
+                },
+                ul: {
+                  marginTop: 0,
+                  marginBottom: theme("spacing.8"),
+                },
+                ol: {
+                  marginTop: 0,
+                  marginBottom: theme("spacing.8"),
+                },
+                "h1, h2, h3, h4, h5, h6": {
+                  marginTop: 0,
+                  marginBottom: 0,
+                  fontWeight: theme("fontWeight.normal"),
+
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    fontWeight: theme("fontWeight.medium"),
+                  },
+                },
+                // tailwind doesn't stick to this property order, so we can't make 'h3' overrule 'h2, h3, h4'
+                "h1, h2": {
+                  fontSize: fontSize("2xl"),
+                  marginTop: theme("spacing.20"),
+                  marginBottom: theme("spacing.10"),
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    fontSize: fontSize("3xl"),
+                  },
+                },
+                "h1:first-child, h2:first-child": {
+                  marginTop: 0,
+                },
+                h3: {
+                  fontSize: fontSize("xl"),
+                  marginTop: theme("spacing.16"),
+                  marginBottom: theme("spacing.10"),
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    fontSize: fontSize("2xl"),
+                  },
+                },
+                "h4, h5, h6": {
+                  fontSize: fontSize("lg"),
+                  [`@media (min-width: ${theme("screens.lg")})`]: {
+                    fontSize: fontSize("xl"),
+                  },
+                },
+                img: {
+                  // images are wrapped in <p>, which already has margin
+                  marginTop: 0,
+                  marginBottom: 0,
+                  borderRadius: theme("borderRadius.lg"),
+                },
+                blockquote: {
+                  fontWeight: theme("fontWeight.normal"),
+                  border: "none",
+                  borderRadius: theme("borderRadius.lg"),
+                  padding: theme("spacing.8"),
+                  marginTop: 0,
+                  marginBottom: theme("spacing.10"),
+                },
+                "blockquote > :last-child": {
+                  marginBottom: 0,
+                },
+              },
+            ],
+          },
+          // use prose-light instead of default, so it's easier to see theme differences
+          light: {
+            css: [
+              {
+                color: theme("colors.body"),
+                a: {
+                  color: theme("colors.primary.500"),
+                },
+                strong: {
+                  color: theme("colors.black"),
+                },
+                hr: {
+                  borderColor: theme("colors.gray.200"),
+                },
+                code: {
+                  color: theme("colors.gray.800"),
+                },
+                "h1, h2, h3, h4, h5, h6, thead th": {
+                  color: theme("colors.black"),
+                },
+                blockquote: {
+                  color: theme("colors.gray.500"),
+                  backgroundColor: theme("colors.gray.100"),
+                },
+                "thead, tbody tr": {
+                  borderBottomColor: theme("colors.gray.200"),
+                },
+              },
+            ],
+          },
+          dark: {
+            css: [
+              {
+                color: theme("colors.body"),
+                a: {
+                  color: theme("colors.primary.500"),
+                },
+                strong: {
+                  color: theme("colors.white"),
+                },
+                hr: {
+                  borderColor: theme("colors.gray.600"),
+                },
+                code: {
+                  color: theme("colors.gray.100"),
+                },
+                "h1, h2, h3, h4, h5, h6, thead th": {
+                  color: theme("colors.white"),
+                },
+                blockquote: {
+                  color: theme("colors.slate.500"),
+                  backgroundColor: theme("colors.gray.800"),
+                },
+                "thead, tbody tr": {
+                  borderBottomColor: theme("colors.gray.600"),
+                },
+              },
+            ],
+          },
+        };
+      },
     },
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [typography, aspectRatio],
 } satisfies Config;
