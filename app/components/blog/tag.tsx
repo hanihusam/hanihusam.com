e31bec14e14fa1f5faf0034d7829af7d445eead0
Hani Husamuddin <hani.husam@gmail.com>
2023-08-01 14:53:13 +0700

    feat: init tag component

---
+++ app/components/blog/tag.tsx
@@ -0,0 +1,37 @@
+import { type ChangeEventHandler } from "react";
+
+import { clsxm } from "@/utils/clsxm";
+
+import { CustomCheckboxContainer, CustomCheckboxInput } from "@reach/checkbox";
+
+interface TagProps {
+  tag: string;
+  selected: boolean;
+  onClick?: ChangeEventHandler<HTMLInputElement>;
+  disabled?: boolean;
+}
+
+function Tag({ tag, selected, onClick, disabled }: TagProps) {
+  return (
+    <CustomCheckboxContainer
+      as="label"
+      checked={selected}
+      onChange={onClick}
+      className={clsxm(
+        "relative mb-4 mr-4 block h-auto w-auto cursor-pointer rounded-full px-6 py-3 transition",
+        {
+          "text-primary bg-secondary": !selected,
+          "text-inverse bg-inverse": selected,
+          "focus-ring opacity-100": !disabled,
+          "opacity-25": disabled,
+        }
+      )}
+      disabled={disabled}
+    >
+      <CustomCheckboxInput checked={selected} value={tag} className="sr-only" />
+      <span>{tag}</span>
+    </CustomCheckboxContainer>
+  );
+}
+
+export { Tag };
