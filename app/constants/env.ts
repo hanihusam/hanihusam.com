8182c820853ced23c396507933b129bb21b3b880
Hani Husamuddin <hani.husam@gmail.com>
2023-10-12 14:22:23 +0700

    feat: incrementMetaFlag

--- app/constants/env.ts
+++ app/constants/env.ts
@@ -1,6 +1,16 @@
+import { getFromLocalStorage } from "@/utils/misc";
+
+export const isProd = process.env.NODE_ENV === "production";
+
 /**
  * Get content meta from the database
  * @see useContentMeta.tsx
  */
-// export const contentMetaFlag = isProd;
-export const contentMetaFlag = true;
+export const contentMetaFlag = isProd;
+
+/**
+ * Increment content views
+ * @see blog.$slug.tsx
+ */
+export const incrementMetaFlag =
+  isProd && getFromLocalStorage("incrementMetaFlag") !== "false";
