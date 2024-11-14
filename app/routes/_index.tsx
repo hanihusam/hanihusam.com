c219c9cae22842585c0ef839e3e0abae22ee85c4
Hani Husamuddin <hani.husam@gmail.com>
2024-11-14 10:47:47 +0700

    feat: project section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ import { AboutSection } from "@/components/home/about-section";
 import { BlogSection } from "@/components/home/blog-section";
 import { CtaSection } from "@/components/home/cta-section";
 import { HeroSection } from "@/components/home/hero-section";
+import { ProjectSection } from "@/components/home/project-section";
 import { ServicesSection } from "@/components/home/services-section";
 import { Spacer } from "@/components/spacer";
 import type { BlogFrontmatter } from "@/types";
@@ -12,6 +13,7 @@ import { getBlogsFeatured } from "@/utils/blog.server";
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
+import projects from "contents/projects";
 
 type LoaderData = {
   featuredPosts: BlogFrontmatter[];
@@ -26,7 +28,7 @@ export const loader: LoaderFunction = async ({ request }) => {
       "for-you-who-wants-to-be-freelancer",
       "why-i-become-freelancer",
     ],
-    { request, timings }
+    { request, timings },
   );
 
   const data: LoaderData = {
@@ -50,6 +52,13 @@ export default function IndexRoute() {
       <AboutSection />
       <Spacer size="lg" />
       <ServicesSection />
+      <Spacer size="lg" />
+      <ProjectSection
+        title="Some of my hand crafts"
+        subTitle="projects"
+        cta="See more projects"
+        projects={projects}
+      />
       <Spacer size="lg" />
       <BlogSection
         title="Find the latest of my writing here"
