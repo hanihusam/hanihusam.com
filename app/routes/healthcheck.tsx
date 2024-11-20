057a907b92f902a0708ae39f2af46b9e450db7dd
Hani Husamuddin <hani.husam@gmail.com>
2024-11-20 09:05:12 +0700

    fix: healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -7,12 +7,12 @@ export async function loader({ request }: LoaderFunctionArgs) {
 		request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 
 	try {
-		const url = new URL('/', `http://${host}`)
-		// if we can connect to the database and make a simple query
-		// and make a HEAD request to ourselves, then we're good.
 		await Promise.all([
 			getContentMdxListItems('blog', { request }),
-			fetch(url.toString(), { method: 'HEAD' }).then((r) => {
+			fetch(`${new URL(request.url).protocol}${host}`, {
+				method: 'HEAD',
+				headers: { 'x-healthcheck': 'true' },
+			}).then((r) => {
 				if (!r.ok) return Promise.reject(r)
 			}),
 		])
